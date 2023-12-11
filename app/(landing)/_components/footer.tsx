75dc26a7ce8bcbdb2b963902130ac99be42473bd
pariwesh <tamrpariwesh@gmail.com>
2023-12-11 21:08:07 +1100

    updated footer component

--- app/(landing)/_components/footer.tsx
+++ app/(landing)/_components/footer.tsx
@@ -5,6 +5,11 @@ const Footer = () => {
     <div className="fixed bottom-0 w-full p-4 border-t bg-bkg/90">
       <div className="md:max-w-screen-xl mx-auto flex items-center w-full justify-between">
         <Logo />
+        <div className="space-x-2 w-auto flex items-center justify-between font-medium text-xs md:text-[16px] text-txt">
+          <span>Technical Exercise</span>
+          <div className="h-6 w-1 bg-orange" />
+          <span className="text-secondary">Pariwesh Tamrakar</span>
+        </div>
       </div>
     </div>
   )
